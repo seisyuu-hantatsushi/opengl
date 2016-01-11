cd68304449b05b84b32c8ad0b747eb2c16f4a9f6
TANIGUCHI, Kazushige <taniguchi.kazushige@gmail.com>
2016-01-11 11:26:18 +0900

    三角形がでました。

--- glut_triangle/main.c
+++ glut_triangle/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 #include <GL/glut.h>
 
 void display(void){
 	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glColor3d(1.0, 0.0, 0.0);
+ 	glVertex2d(0.9*cos(0.0), 0.9*sin(0.0));
+	glColor3d(0.0, 1.0, 0.0);
+	glVertex2d(0.9*cos(2.0*M_PI/3.0), 0.9*sin(2.0*M_PI/3.0));
+	glColor3d(0.0, 0.0, 1.0);
+	glVertex2d(0.9*cos(2.0*M_PI*2.0/3.0), 0.9*sin(2.0*M_PI*2.0/3.0));
+	glEnd();
 	glFlush();
 	return;
 }
 
 void init(void){
-	glClearColor(0.0, 0.0, 1.0, 1.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	return;
 }
 

0eb5da2173bcde60d5d18d702e9db5b64f4b3ee8
TANIGUCHI, Kazushige <taniguchi.kazushige@gmail.com>
2016-01-15 02:08:33 +0900

    三角形が描画されるまで.

--- glfw_triangle/main.c
+++ glfw_triangle/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, char *argv[]){
@@ -8,8 +9,22 @@ int main(int argc, char *argv[]){
 	}
 
 	pWindow = glfwCreateWindow(640, 480, "Triangle", NULL, NULL);
+
+	glfwMakeContextCurrent(pWindow);
 	
 	while(!glfwWindowShouldClose(pWindow)){
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+		glColor3d(1.0, 0.0, 0.0);
+		glVertex2d(0.9*cos(0.0), 0.9*sin(0.0));
+		glColor3d(0.0, 1.0, 0.0);
+		glVertex2d(0.9*cos(2.0*M_PI/3.0), 0.9*sin(2.0*M_PI/3.0));
+		glColor3d(0.0, 0.0, 1.0);
+		glVertex2d(0.9*cos(2.0*M_PI*2.0/3.0), 0.9*sin(2.0*M_PI*2.0/3.0));
+		glEnd();
+		glFlush();
+		glfwSwapBuffers(pWindow);
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(pWindow);

94a4c28eac14b2cc1e1d6fd488c3ea1311b6c645
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2018-05-16 00:14:52 +0900

    gtkのopenglサンプルを作成するために、gtkの単純なプログラムを追加.

---
+++ prototypes/gtk/quat/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+int main(int argc, char **argv){
+	GtkWidget *window;
+
+	gtk_init(&argc, &argv);
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	gtk_widget_set_size_request(window, 300, 200);
+
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_widget_show(window);
+
+	gtk_main();
+
+	return 0;
+}
